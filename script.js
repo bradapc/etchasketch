1ab0611c93c729a70c5486a7bd6f4d122aae4dce
Brad <brad.cocar@gmail.com>
2021-11-26 16:20:03 -0800

    Added drawing on mouseover

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let container = document.querySelector('.container');
 for(i = 0; i < 256; i++) {
     let gridBox = document.createElement('div');
+    gridBox.classList.add('grid-box');
     gridBox.style.height = '25px';
     gridBox.style.width = '25px';
-    gridBox.style.backgroundColor = 'red';
+    gridBox.style.backgroundColor = 'grey';
     gridBox.style.flex = "0 0 6.25%";
     container.appendChild(gridBox);
 }
+
+let box = document.querySelectorAll('.grid-box')
+box.forEach(boxListen => boxListen.addEventListener('mouseover', () => {
+    boxListen.style.backgroundColor = "red";
+}));
