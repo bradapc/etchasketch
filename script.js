cf1322763d9bc9307c1f4ee7d82e0287e578c603
Brad <brad.cocar@gmail.com>
2021-11-26 17:00:48 -0800

    Added ability to clear board and specify a new number of tiles for each side

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let container = document.querySelector('.container');
-function makeGrid() {
-    for(i = 0; i < 256; i++) {
+function makeGrid(numTiles) {
+    let totalBoxes = numTiles * numTiles; //# of boxes in grid
+    let areaOfBoxes = 160000 / totalBoxes; //160000 from 400x400 container
+    let boxDimension = Math.sqrt(areaOfBoxes);
+    let boxFlexBasis = 1 / numTiles * 100;
+    console.log(boxFlexBasis);
+    for(i = 0; i < totalBoxes; i++) {
         let gridBox = document.createElement('div');
         gridBox.classList.add('grid-box');
-        gridBox.style.height = '25px';
-        gridBox.style.width = '25px';
+        gridBox.style.height = boxDimension;
+        gridBox.style.width = boxDimension;
         gridBox.style.backgroundColor = 'grey';
-        gridBox.style.flex = "0 0 6.25%";
+        gridBox.style.flexBasis = `${boxFlexBasis}%`;
         container.appendChild(gridBox);
     }
     let box = document.querySelectorAll('.grid-box')
@@ -22,7 +27,16 @@ clearButton.addEventListener('click', reset);
 function reset() {
     let gridBox = document.querySelectorAll('.container div');
     gridBox.forEach(removeBox => removeBox.remove());
-    makeGrid();
+    makeGrid(newGridSize());
 }
 
-makeGrid();
+function newGridSize() {
+    let newSize = 0;
+    do {
+        newSize = prompt("Enter the number tiles you want per side (Max 100)");
+        newSize = Math.round(newSize);
+    } while(newSize <= 0 || newSize > 100)
+    return newSize;
+}
+
+makeGrid(16);
