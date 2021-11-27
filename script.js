777e8e0c7852e7884834d2f81eda6fd19b483df1
Brad <brad.cocar@gmail.com>
2021-11-26 16:33:45 -0800

    Added the ability to clear the grid

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 let container = document.querySelector('.container');
-for(i = 0; i < 256; i++) {
-    let gridBox = document.createElement('div');
-    gridBox.classList.add('grid-box');
-    gridBox.style.height = '25px';
-    gridBox.style.width = '25px';
-    gridBox.style.backgroundColor = 'grey';
-    gridBox.style.flex = "0 0 6.25%";
-    container.appendChild(gridBox);
+function makeGrid() {
+    for(i = 0; i < 256; i++) {
+        let gridBox = document.createElement('div');
+        gridBox.classList.add('grid-box');
+        gridBox.style.height = '25px';
+        gridBox.style.width = '25px';
+        gridBox.style.backgroundColor = 'grey';
+        gridBox.style.flex = "0 0 6.25%";
+        container.appendChild(gridBox);
+    }
+    let box = document.querySelectorAll('.grid-box')
+    box.forEach(boxListen => boxListen.addEventListener('mouseover', () => {
+        boxListen.style.backgroundColor = "red";
+    }));
 }
 
-let box = document.querySelectorAll('.grid-box')
-box.forEach(boxListen => boxListen.addEventListener('mouseover', () => {
-    boxListen.style.backgroundColor = "red";
-}));
+
+let clearButton = document.querySelector('.reset');
+clearButton.addEventListener('click', reset);
+
+function reset() {
+    let gridBox = document.querySelectorAll('.container div');
+    gridBox.forEach(removeBox => removeBox.remove());
+    makeGrid();
+}
+
+makeGrid();
